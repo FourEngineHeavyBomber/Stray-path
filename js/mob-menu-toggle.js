0008e9a2a56a3c99271fc03d4d4bb39d695221c8
AntonHuggard <56659693+AntonHuggard@users.noreply.github.com>
2021-07-11 14:54:06 +1200

    Added js

---
+++ js/mob-menu-toggle.js
@@ -0,0 +1,11 @@
+function fn() {
+    document.getElementById('mob-nav-menu').classList.add('show-me')
+    document.getElementById('mob-nav-menu').classList.remove('hide-me')
+    document.getElementById('hamburger-bar').classList.add('hide-me');
+}
+function undo() {
+    document.getElementById('mob-nav-menu').classList.remove('show-me')
+    document.getElementById('mob-nav-menu').classList.add('hide-me')
+    document.getElementById('hamburger-bar').classList.remove('hide-me');
+    document.getElementById('hamburger-bar').classList.add('show-me');
+}
