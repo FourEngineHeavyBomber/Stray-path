89f1e2c71823fc99ad5fcbcaa5d32bbe0ca3cf98
AntonHuggard <56659693+AntonHuggard@users.noreply.github.com>
2021-08-01 17:38:06 +1200

    Improved img loading

--- js/embiggen.js
+++ js/embiggen.js
@@ -13,3 +13,12 @@ document.addEventListener('keydown', (event) => {
             unembiggen();
     }
 });
+
+function embiggener(element) {
+    var myPath = element.src;
+    var ftIndex = myPath.indexOf("small");
+    var sdIndex = myPath.lastIndexOf("/");
+    result = myPath.substring(0, ftIndex) + 'stills' + myPath.substring(sdIndex);
+    document.getElementById('modal-container').style.display = 'block';
+    document.getElementById('modal-img').src = result;
+}
