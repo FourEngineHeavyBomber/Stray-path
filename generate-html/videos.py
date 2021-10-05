2f8d95f2ef799d3aa7aea98abbb26702cc996827
AntonHuggard <56659693+AntonHuggard@users.noreply.github.com>
2021-10-05 21:04:07 +1300

    Finished code for youtube

--- generate-html/videos.py
+++ generate-html/videos.py
@@ -8,14 +8,36 @@ add_video = True
 while add_video:
     cont = input("add video? (t/f): ")
     if cont.lower() == "t":
+
+        video_title = input("enter video title: ")
+        lcHtml = lcHtml + "<h3 class='video-title'>" + video_title + "</h3>"
+        lcHtml = lcHtml + "\n"
+        
         website = input("is this video from youtube or vimeo? (yt/vm): ")
         if website.lower() == "yt":
-            link = input("paste link here: ")
+            
+            link = input("paste video ID - not link - here (for explanation enter H): ")
+            while link.lower() == "h":
+                print("youtube videos have an ID you can grab from their URL")
+                print("in this >> https://youtu.be/tSbSkGoOO8M, the ID is tSbSkGoOO8M")
+                print("in this >> https://www.youtube.com/watch?v=2Jn1OeDlNpM&ab_channel=EleannaSiozou")
+                print("the ID is 2Jn1OeDlNpM")
+                print("it's important you exclude v= and the &channel data")
+                link = input("paste video ID here (this is for SEO, it will not be visible): ")
             name = input("give the video a name (one word): ")
             name = name.split(" ")[0]
             lkHtml = "<iframe class='video centre' name='" + name + "' src='"
-            lkHtml =  lkHtml + link + "' > \n </iframe>\n\n"
+            lkHtml =  lkHtml + "https://www.youtube.com/embed/" + link
+            lkHtml =  lkHtml + "' > \n </iframe>\n\n"
             lcHtml = lcHtml + lkHtml
+
+            description = input("enter video description: ")
+            lcHtml = lcHtml + "<p class='video-description'>" + description
+            lcHtml = lcHtml + "</p>"
+
+            lcHtml = lcHtml + "\n"
+
+            
         elif website.lower() == "vm":
             link = input("paste link here: ")
             name = input("give the video a name (one word): ")
